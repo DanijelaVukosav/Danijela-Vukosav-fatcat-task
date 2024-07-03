623375c62a99ca9ed8b5af1029d23e9249e96913
Danijela Vukosav <danijela99vukosav@gmail.com>
2024-07-03 12:58:46 +0200

    refactor(vite.config): Delete variables
    Delete unusable variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import path from 'path';
-export default defineConfig(({ command, mode }) => {
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const env = loadEnv(mode, process.cwd(), '');
-    console.log('env1', env);
-    console.log('env2', process.cwd());
-    const __CWD__ = process.cwd();
-
+export default defineConfig(() => {
     return {
-        // vite config
         plugins: [react()],
         resolve: {
             alias: [
@@ -21,9 +13,7 @@ export default defineConfig(({ command, mode }) => {
             ],
         },
         define: {
-            __APP_ENV__: env.APP_ENV,
             __CWD__: JSON.stringify(process.cwd()),
-            VITE_APP_YOUR_KEY: JSON.stringify(process.cwd()),
         },
     };
 });
